338db78394a8aabecf31e6e8b129b05c3634a0b2
zaydzuhri <zaydzuhri@gmail.com>
2023-04-20 14:11:54 +0700

    fix: stop animating (buggy)

--- src/animation/animation.js
+++ src/animation/animation.js
@@ -30,23 +30,52 @@ let frameIdx = 0;
 
 assignAnimObjectReference();
 
+var stop = false;
+var frameCount = 0;
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+
+// initialize the timer variables and start the animation
+
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
 async function animate() {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    nextFrame = anim[frameIdx];
+    // request another frame
 
-    state.model = nextFrame;
-    treeview.select("root");
-    treeview.destroy();
-    treeview.replaceData(root);
-    appendDataFromObject(state.model.object, "root");
-    treeview.select("root");
+    // calc elapsed time since last loop
 
-    frameIdx = (frameIdx + 1) % anim.length;
+    now = Date.now();
+    elapsed = now - then;
 
-    drawScene();
+    // if enough time has elapsed, draw the next frame
 
-    animationID = requestAnimationFrame(animate);
+    if (elapsed > 500) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % 500);
+
+        // Put your drawing code here
+        nextFrame = anim[frameIdx];
+
+        state.model = nextFrame;
+        treeview.select("root");
+        treeview.destroy();
+        treeview.replaceData(root);
+        appendDataFromObject(state.model.object, "root");
+        treeview.select("root");
+
+        frameIdx = (frameIdx + 1) % anim.length;
+
+        drawScene();
+    }
     console.log("Update animation ID:" + animationID);
+    animationID = requestAnimationFrame(animate);
 }
 
 const toggleSwitchAnimation = document.getElementById("switch-animation");
@@ -55,7 +84,7 @@ toggleSwitchAnimation.addEventListener("change", function () {
         // do something when the switch is on
         document.getElementById("on-off-label-animation").innerHTML = "ON";
         anim = state.model.animation;
-        animationID = requestAnimationFrame(animate);
+        startAnimating(2)
     } else {
         // do something when the switch is off
         document.getElementById("on-off-label-animation").innerHTML = "OFF";
